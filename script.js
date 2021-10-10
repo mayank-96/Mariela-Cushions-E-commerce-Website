c8cb4aea30475069c452041b22a5c361a50c13b3
mayank-96 <pagarmayank07@gmail.com>
2021-10-10 12:55:59 +0530

    Completed: Navigation

--- script.js
+++ script.js
@@ -30,3 +30,28 @@ function toggleMenu() {
     menu.replace("close", "open");
   }
 }
+
+// CHANGE HERO IMAGE
+
+function changeBanner(direction) {
+  const banner = [
+    "./images/hero-banner-1.jpg",
+    "./images/hero-banner-2.jpg",
+    "./images/hero-banner-3.jpg",
+    "./images/hero-banner-4.jpg",
+  ];
+  const currBanner =
+    document.getElementById("hero-banner").attributes.src.value;
+  var index = banner.indexOf(currBanner);
+
+  if (direction === "next") {
+    index = (index + 1) % banner.length;
+  } else {
+    index = index - 1;
+    if (index === -1) {
+      index = banner.length - 1;
+    }
+  }
+
+  document.getElementById("hero-banner").attributes.src.value = banner[index];
+}
